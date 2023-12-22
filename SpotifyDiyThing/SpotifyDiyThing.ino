cebfac1bd24fdbdf4a5de1003ef86df11ebe43fe
Bastian Baumeister <ich@bastianbaumeister.de>
2023-12-22 15:57:53 +0100

    fixed type

--- SpotifyDiyThing/SpotifyDiyThing.ino
+++ SpotifyDiyThing/SpotifyDiyThing.ino
@@ -15,7 +15,7 @@
 // (Uncomment the required #define)
 
 // 1. Cheap yellow display (Using TFT-eSPI library)
-define YELLOW_DISPLAY
+#define YELLOW_DISPLAY
 
 // 2. Matrix Displays (Like the ESP32 Trinity)
 // #define MATRIX_DISPLAY
